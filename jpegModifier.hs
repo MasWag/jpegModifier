9dc0553cd0222716f11a6b470854387a6d6c3636
Masaki Waga <tsugarutamenobu@gmail.com>
2013-02-21 14:35:09 +0900

    inserting SOI and EOI seems to be done

--- jpegModifier.hs
+++ jpegModifier.hs
@@ -1,6 +1,18 @@
-import Data.Char
+import qualified Data.ByteString as B
 import System.Environment
 import System.IO
+
+main = do
+  args <- getArgs
+  if null args
+    then putStrLn "Error! Usage : jpgCheck filename"
+    else do
+    contents <- B.readFile (head args)
+    if jpgCheck $ B.unpack contents then
+      putStrLn "input jpeg file was valid!"
+      else do
+      B.writeFile ((head args) ++ ".modified.jpeg") $ (if checkSOI $ B.unpack contents then \x->x else addSOI) ( ( if checkEOI $ B.unpack contents then \x->x else addEOI ) contents)
+      putStrLn $ "output as" ++  ((head args) ++ ".modified.jpeg")
 
 checkSOI (x:[]) = False
 checkSOI (x:x':_) = x == 0xFF && x' == 0xD8
@@ -12,12 +24,6 @@ checkEOI xs = checkEOI' $ reverse xs
 
 jpgCheck xs = checkSOI xs && checkEOI xs
 
-main = do
-        args <- getArgs
-        if null args
-            then putStrLn "Error! Usage : jpgCheck filename"
-            else do
-                h <- openBinaryFile (head args) ReadMode
-                cs <- hGetContents h
-                print $ jpgCheck $ map ord cs
-                if jpgCheck $ map ord cs
+addSOI xs = B.cons 0xFF $ B.cons 0xD8 xs
+
+addEOI xs = B.snoc (B.snoc xs  0xFF) 0xD9
